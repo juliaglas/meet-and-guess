c7f5fed4331f96b94fd566ca3b165b9b642cbe1e
Julia Glas <julia.glas@tum.de>
2014-12-06 12:01:36 +0100

    Added activity and layout for questions.

---
+++ src/edu/chalmers/meetandguess/QuestionActivity.java
@@ -0,0 +1,13 @@
+package edu.chalmers.meetandguess;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.question);
+	}
+}
