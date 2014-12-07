cd0d2a1f159ca3f57d42b2f09d924811fb7aca4e
Vasileios <v.golematis@gmail.com>
2014-12-07 18:26:27 +0100

    Added toolbar and Navigation Drawer with simple Array List Adapter

---
+++ src/edu/chalmers/meetandguess/Navigation.java
@@ -0,0 +1,26 @@
+package edu.chalmers.meetandguess;
+
+import java.util.ArrayList;
+
+
+
+public class Navigation {
+	
+	private ArrayList<String> navList;
+	
+	public Navigation()
+	{
+		navList = new ArrayList<String>();
+		navList.add("Profile Settings");
+		navList.add("View Players");
+		navList.add("Create New Game");
+		navList.add("Join Game");
+		
+	}
+	
+	public ArrayList<String> getNavList()
+	{
+		return navList;
+	}
+
+}
