a31ad7f875a16b0bb22f516f72b729663af3b082
Julia Glas <julia.glas@tum.de>
2014-12-08 15:11:51 +0100

    Added a profile activity to display and change user information.

---
+++ src/edu/chalmers/meetandguess/ProfileActivity.java
@@ -0,0 +1,19 @@
+package edu.chalmers.meetandguess;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ProfileActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile);
+
+		// Toolbar Layout
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setTitle(getResources().getText(R.string.profile));
+	}
+}
